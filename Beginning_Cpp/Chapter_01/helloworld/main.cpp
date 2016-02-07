e2755d1fae6ef82a7c90e52bf5960f8d33710b16
Frank <fwoodling@gmail.com>
2016-02-07 01:15:49 -0500

    Chapter 2

---
+++ Beginning_Cpp/Chapter_01/helloworld/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// preprocessor directive starts with #
+// This runs before the compiler
+// Will include iostream
+// < > tells the compiler to find the file where the compiler files are kept
+
+// Game Over
+// A first C++ program
+
+/* Can also use a C style comment
+ * It covers multiple lines */
+
+int main()
+{
+    std::cout << "Game Over" << std::endl;
+    return 0;
+}
+
