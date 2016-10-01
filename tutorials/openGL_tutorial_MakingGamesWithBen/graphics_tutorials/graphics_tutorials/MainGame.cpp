0f478d262b5cab6df096e46e21d27ec3869b795e
Frank Woodling <Frank Woodling>
2016-10-01 01:39:55 -0400

    add drawing and error catching

--- tutorials/openGL_tutorial_MakingGamesWithBen/graphics_tutorials/graphics_tutorials/MainGame.cpp
+++ tutorials/openGL_tutorial_MakingGamesWithBen/graphics_tutorials/graphics_tutorials/MainGame.cpp
@@ -1,5 +1,14 @@
 #include "MainGame.h"
 #include <iostream>
+#include <string>
+
+void fatalError(std::string errorString) {
+	std::cout << errorString << std::endl;
+	std::cout << "Enter any key to  quit...";
+	int tmp;
+	std::cin >> tmp;
+	SDL_Quit();
+}
 
 MainGame::MainGame()
 {
@@ -25,11 +34,30 @@ void MainGame::initSystems() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	_window = SDL_CreateWindow("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_OPENGL);
+	
+	if (_window == nullptr) {
+		fatalError("SDL Window could not be created!");
+	}
+
+	SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+	if (glContext == nullptr) {
+		fatalError("SDL_GL context could not be created!");
+	}
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		fatalError("Could not initialize glew!");
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0);
 }
 
 void MainGame::gameLoop() {
 	while (_gameState != GameState::EXIT) {
 		processInput();
+		drawGame();
 	}
 }
 
@@ -47,4 +75,20 @@ void MainGame::processInput() {
 			break;
 		}
 	}
+}
+
+void MainGame::drawGame() {
+	glClearDepth(1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnableClientState(GL_COLOR_ARRAY);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0, 0);
+	glVertex2f(0, 500);
+	glVertex2f(500, 500);
+
+	glEnd();
+
+	SDL_GL_SwapWindow(_window);
 }
